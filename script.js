1e62c662e6adae1f9174956621c82dfa0caf4981
Aditya Raj Jha <88044291+Aadi0509@users.noreply.github.com>
2024-06-17 15:46:44 +0530

    Add files via upload
    css and js files

---
+++ script.js
@@ -0,0 +1,66 @@
+let history = [];
+
+const canvasColor = document.getElementById('canvasColor');
+const canvas = document.getElementById('myCanvas');
+const undoButton = document.getElementById('undoButton');
+const ClrBtn = document.getElementById('ClrBtn');
+const SaveBtn = document.getElementById('SaveBtn');
+const fontPicker = document.getElementById('fontPicker');
+const textInput = document.getElementById('textInput');
+
+
+const ctx = canvas.getContext('2d');
+
+colorPicker.addEventListener('change', (event) => {
+    ctx.fillStyle = event.target.value;
+    ctx.strokeStyle = event.target.value;
+});
+
+canvasColor.addEventListener('change', (event) => {
+    ctx.fillStyle = event.target.value;
+    ctx.fillRect(0, 0, 800, 500);
+});
+
+canvas.addEventListener('mousedown', (event) => {
+    isDrawing = true;
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+});
+
+canvas.addEventListener('mousemove', (event) => {
+    if (isDrawing) {
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        
+        lastX = event.offsetX;
+        lastY = event.offsetY;
+    }
+});
+
+canvas.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+});
+
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+fontSizePicker.addEventListener('change', (event) => {
+    ctx.lineWidth = event.target.value;
+});
+
+ClrBtn.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
+
+SaveBtn.addEventListener('click', () => {
+    localStorage.setItem('canvasContents', canvas.toDataURL());
+    let link = document.createElement('a');
+    
+    link.download = 'my-canvas.png';
+    link.href = canvas.toDataURL();
+    
+    link.click();
+});
